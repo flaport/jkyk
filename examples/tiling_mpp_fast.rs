38b3a0e034d87e6e7fc41ec3e5a87714885d9de9
flaport <floris.laporte@gmail.com>
2025-06-06 14:40:46 +0200

    tiling mpp fast is working

--- examples/tiling_mpp_fast.rs
+++ examples/tiling_mpp_fast.rs
@@ -7,9 +7,9 @@ const N: isize = 96;
 const P: isize = 64;
 const C: isize = 3;
 const F: isize = 2;
+const Q: isize = 96;
 const W: isize = 8;
-const H: isize = W - 2;
-const Q: isize = 16 * H;
+const H: isize = 6;
 const W2: isize = W / 2;
 const W32: isize = 3 * W2;
 
@@ -35,7 +35,7 @@ fn ix(m: isize, n: isize, p: isize, c: isize, f: isize) -> usize {
 
 #[inline]
 fn wix(m: isize, n: isize, p: isize, c: isize, f: isize) -> usize {
-    ((((f * W + m) * W32 + n) * W32 + p) * C + c) as usize // no bounds wrapping needed
+    ((((f * W + (m + W) % W) * W32 + (n + W32) % W32) * W32 + (p + W32) % W32) * C + c) as usize
 }
 
 fn main() -> Result<()> {
@@ -55,23 +55,18 @@ fn main() -> Result<()> {
     let mut j0: isize;
     let mut j1: isize;
     let mut j2: isize;
-
-    // NOTE: when W == H, the fast array dimension size for mountain valley dimensions
-    // should be of size W + 1. in that case also the wix() function should be adjusted
-    // to use W+1 instead as well.
     let mut fast = [0_f32; (W * W32 * W32 * C * F) as usize];
 
     for mut i in 0..(2 * Q / H) {
         i *= H;
         for mvm in 0..MV {
-            // mvm: 0: mountain, 1: valley (m-direction)
             for mut om in 0..oms {
                 om *= W; // om: offset in x/m direction
                 for mut on in 0..ons {
                     on *= W; // on: offset in y/n direction
                     for mut op in 0..ops {
                         op *= W; // op: offset in z/p direction
-                        fill_fast(&mut fast, &eh, om, mvm, on, op);
+                        fill_fast(&mut fast, &eh, om, on, op, mvm);
                         for h in 0..H {
                             f = h % 2;
                             g = 1 - f;
@@ -80,10 +75,10 @@ fn main() -> Result<()> {
                             // global coords
                             let gm0 = om + (1 - mvm) * (h / 2 + 1) + mvm * (W - (h + 1) / 2);
                             let gm1 = om + (1 - mvm) * (W - (h + 1) / 2) + mvm * (W + 1 + h / 2);
-                            let gn0 = on + W - (h + 1) / 2;
-                            let gn1 = on + 2 * W - (h + 1) / 2;
-                            let gp0 = op + W - (h + 1) / 2;
-                            let gp1 = op + 2 * W - (h + 1) / 2;
+                            let gn0 = on - (h + 1) / 2;
+                            let gn1 = on + W - (h + 1) / 2;
+                            let gp0 = op - (h + 1) / 2;
+                            let gp1 = op + W - (h + 1) / 2;
 
                             // local coords
                             let m0 = (1 - mvm) * (h / 2 + 1) + mvm * (W2 - (h + 1) / 2);
@@ -93,21 +88,20 @@ fn main() -> Result<()> {
                             let p0 = W2 - (h + 1) / 2;
                             let p1 = W32 - (h + 1) / 2;
 
-                            // update tile
                             for (m, gm) in (m0..m1).zip(gm0..gm1) {
                                 for (n, gn) in (n0..n1).zip(gn0..gn1) {
                                     for (p, gp) in (p0..p1).zip(gp0..gp1) {
                                         for c0 in 0..C {
                                             c1 = (c0 + 1) % 3;
                                             c2 = (c0 + 2) % 3;
-                                            j0 = s * ((c0 == 0) as isize); // positive for E [f=0], negative for H [f=1]
-                                            j1 = s * ((c0 == 1) as isize); // positive for E [f=0], negative for H [f=1]
-                                            j2 = s * ((c0 == 2) as isize); // positive for E [f=0], negative for H [f=1]
-                                            eh[ix(m, n, p, c0, f)] += sc
-                                                * (eh[ix(m, n, p, c2, g)]
-                                                    - eh[ix(m + j2, n + j0, p + j1, c2, g)]
-                                                    - eh[ix(m, n, p, c1, g)]
-                                                    + eh[ix(m + j1, n + j2, p + j0, c1, g)]);
+                                            j0 = s * ((c0 == 0) as isize); // negative for E [f=0], positive for H [f=1]
+                                            j1 = s * ((c0 == 1) as isize); // negative for E [f=0], positive for H [f=1]
+                                            j2 = s * ((c0 == 2) as isize); // negative for E [f=0], positive for H [f=1]
+                                            fast[wix(m, n, p, c0, f)] += sc
+                                                * (fast[wix(m, n, p, c2, g)]
+                                                    - fast[wix(m + j2, n + j0, p + j1, c2, g)]
+                                                    - fast[wix(m, n, p, c1, g)]
+                                                    + fast[wix(m + j1, n + j2, p + j0, c1, g)]);
                                         }
                                         if f == 0 && gm == SX && gn == SY && gp == SZ {
                                             fast[wix(m, n, p, 2, 0)] += st[(i + h) as usize / 2];
@@ -116,7 +110,7 @@ fn main() -> Result<()> {
                                 }
                             }
                         }
-                        extract_fast(&mut eh, &fast, om, mvm, on, op);
+                        extract_fast(&mut eh, &fast, om, on, op, mvm);
                     }
                 }
             }
@@ -137,7 +131,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn fill_fast(fast: &mut [f32], eh: &[f32], om: isize, mvm: isize, on: isize, op: isize) {
+fn fill_fast(fast: &mut [f32], eh: &[f32], om: isize, on: isize, op: isize, mvm: isize) {
     fast.fill(0.0);
     let dwm = mvm * W2;
     for f in 0..F {
@@ -160,7 +154,7 @@ fn fill_fast(fast: &mut [f32], eh: &[f32], om: isize, mvm: isize, on: isize, op:
     }
 }
 
-fn extract_fast(eh: &mut [f32], fast: &[f32], om: isize, mvm: isize, on: isize, op: isize) {
+fn extract_fast(eh: &mut [f32], fast: &[f32], om: isize, on: isize, op: isize, mvm: isize) {
     let dwm = mvm * W2;
     for f in 0..F {
         for (i, m) in (om + dwm..om + W + dwm).enumerate() {
